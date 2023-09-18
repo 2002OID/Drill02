260d2f5ecfa37265825be9d8a0b08e3af0b5ad22
2002OID <ykkim308321@gmail.com>
2023-09-18 14:51:12 +0900

    make move rec

--- character_grass.py
+++ character_grass.py
@@ -4,12 +4,25 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
-x = 0
-while (x < 800):
+
+x=400
+y=90
+while (True):
  clear_canvas_now()
  grass.draw_now(400, 30)
- character.draw_now(x, 90)
- x = x + 2
+ character.draw_now(x, y)
+ if y == 90:
+     if x < 800:
+         x = x+2
+ if x == 800:
+     if y < 600:
+         y = y + 2
+ if y == 600:
+     if x > 0:
+         x = x - 2
+ if x == 0:
+     if y > 90:
+         y = y - 2
  delay(0.01)
 
 close_canvas()
