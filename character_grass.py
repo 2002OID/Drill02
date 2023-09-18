d02852ce4d055d7e35f79849910719146274188f
2002OID <ykkim308321@gmail.com>
2023-09-18 14:54:08 +0900

    divied move

--- character_grass.py
+++ character_grass.py
@@ -7,22 +7,30 @@ character = load_image('character.png')
 
 x=400
 y=90
+move_ment = 0
+
 while (True):
  clear_canvas_now()
  grass.draw_now(400, 30)
  character.draw_now(x, y)
- if y == 90:
-     if x < 800:
-         x = x+2
- if x == 800:
-     if y < 600:
-         y = y + 2
- if y == 600:
-     if x > 0:
-         x = x - 2
- if x == 0:
-     if y > 90:
+ if move_ment == 0:
+  if y == 90:
+      if x < 800:
+          x = x+2
+  if x == 800:
+      if y < 600:
+          y = y + 2
+  if y == 600:
+      if x > 0:
+          x = x - 2
+  if x == 0:
+      if y > 90:
          y = y - 2
- delay(0.01)
+  if x == 400:
+      if y == 90:
+          move_ment = 1
+ if move_ment == 1:
+     
+delay(0.01)
 
 close_canvas()
